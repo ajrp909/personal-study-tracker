3763b880e12c1729be9ebbabfb601061529ac525
ajrp909 <146494885+ajrp909@users.noreply.github.com>
2025-01-24 19:59:34 +0000

    major revamp due to heroku

--- src/main.py
+++ src/main.py
@@ -1,16 +1,29 @@
+import os
+
+import psycopg2
+
 from fastapi import FastAPI, HTTPException
 
 from pydantic import BaseModel
 
 from datetime import datetime
 
+from dotenv import load_dotenv
+
 from src.utils import new_csv, csv_formatter, update_csv
 
-CSV_FILEPATH = "./src/questions.csv"
+load_dotenv()
 
-NUMBER_OF_QUESTIONS = 500
+DATABASE_URL = os.environ['DATABASE_URL']
 
-EXAM_DATE = datetime.strptime('5 Jun 2025','%d %b %Y')
+NUMBER_OF_QUESTIONS: int = 500
+
+EXAM_DATE: datetime = datetime.strptime('5 Jun 2025','%d %b %Y')
+
+questions_string = ""
+for i in range(1, NUMBER_OF_QUESTIONS):
+    questions_string += f"({i}),"
+questions_string += f"({NUMBER_OF_QUESTIONS})"
 
 class Question(BaseModel):
     question_id: int
@@ -18,11 +31,24 @@ class Question(BaseModel):
     correct: bool
     date: datetime
 
-questions = csv_formatter(CSV_FILEPATH)
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+cursor = conn.cursor()
 
-if not questions:
-    new_csv(CSV_FILEPATH, NUMBER_OF_QUESTIONS)
-    questions = csv_formatter(CSV_FILEPATH)
+cursor.execute("""create table if not exists public.questions (
+    question_id integer primary key,
+    difficulty integer not null default 0,
+    correct boolean,    
+    date date);
+               """
+               )
+conn.commit()
+
+cursor.execute(f"""insert into public.questions
+                values {questions_string};
+""")
+conn.commit()
+cursor.close()
+conn.close()
 
 app = FastAPI()
 
@@ -41,17 +67,17 @@ async def question_summary():
             f"days left: {days_left}",
             f"questions per day: {questions_per_day}")
 
-@app.get("/question/{question_id}")
-async def question_id_call(question_id: int):
-    if 0 < question_id < len(questions):
-        return questions[question_id]
-    raise HTTPException(status_code=404, detail="Question {question_id} not found")
+# @app.get("/question/{question_id}")
+# async def question_id_call(question_id: int):
+#     if 0 < question_id < len(questions):
+#         return questions[question_id]
+#     raise HTTPException(status_code=404, detail="Question {question_id} not found")
 
-@app.put("/question/{question_id}/{difficulty}/{correct}")
-async def question_update(question_id: int, difficulty: int, correct: bool):
-    if 0 < question_id < len(questions):
-        question_update_string = f"{question_id},{difficulty},{correct},{datetime.now()}"
-        questions[question_id] = question_update_string
-        update_csv(CSV_FILEPATH, questions)
-        return f"Question {question_id} was updated to {question_update_string}"
-    raise HTTPException(status_code=404, detail="Question {question_id} not found")
+# @app.put("/question/{question_id}/{difficulty}/{correct}")
+# async def question_update(question_id: int, difficulty: int, correct: bool):
+#     if 0 < question_id < len(questions):
+#         question_update_string = f"{question_id},{difficulty},{correct},{datetime.now()}"
+#         questions[question_id] = question_update_string
+#         update_csv(CSV_FILEPATH, questions)
+#         return f"Question {question_id} was updated to {question_update_string}"
+#     raise HTTPException(status_code=404, detail="Question {question_id} not found")
