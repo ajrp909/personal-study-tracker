51702d94cb5323a563b095ca42000f6cc0193c9f
ajrp909 <146494885+ajrp909@users.noreply.github.com>
2025-01-25 14:00:23 +0000

    make it better

--- src/database.py
+++ src/database.py
@@ -1,35 +1,104 @@
+import os
 import psycopg2
+from psycopg2 import sql
+from dotenv import load_dotenv
 
-def create_database(DATABASE_URL):
-    with psycopg2.connect(DATABASE_URL, sslmode='require') as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("""create table if not exists public.questions (
-                                question_id integer primary key,
-                                difficulty integer not null default 0,
-                                correct boolean,    
-                                date date);
-                           """)
-            
-def seed_database(DATABASE_URL, questions_string):
-    with psycopg2.connect(DATABASE_URL, sslmode='require') as conn:
-        with conn.cursor() as cursor:
-            cursor.execute(f"""insert into public.questions
-                values {questions_string};
-                """)
-            
-def drop_database(DATABASE_URL):
-    with psycopg2.connect(DATABASE_URL, sslmode='require') as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("""drop table questions;
-                           """)
-            
-def update_database(DATABASE_URL, question_update_list):
-    question_id, difficulty, correct, date = question_update_list
-    with psycopg2.connect(DATABASE_URL, sslmode='require') as conn:
-        with conn.cursor() as cursor:
-            cursor.execute(f"""update questions
-                           set difficulty = {difficulty},
-                           correct = {correct},
-                           date = {date}
-                            where question_id = {question_id}
-                            """)
+from datetime import datetime
+
+load_dotenv()
+
+TABLE_NAME = "question"
+DATABASE_URL = os.environ['DATABASE_URL']
+DB_CONN = "{DATABASE_URL}, sslmode='require'"
+
+def sql_create_table():
+    """Connects to a database and tries to create a table using the table_name parameter.
+    Returns a string letting the user know if successful or unsuccessful.
+    """
+    try:
+        table_name = TABLE_NAME
+        query = "create table {} (question_id integer primary key, difficulty integer not null default 0, correct boolean, date date);"
+        conn = psycopg2.connect(DB_CONN)
+        cur = conn.cursor()
+        cur.execute(sql.SQL(query).format(sql.Identifier(table_name)))
+        conn.commit()
+        cur.close()
+        conn.close()
+        print(f"{table_name} table created successfully.")
+    except Exception:
+        print(f"{table_name} table already exists.")
+
+def sql_seed_table(questions):
+    try:
+        table_name = TABLE_NAME
+        query = "insert into {} values (%s)"
+        placeholder_list = [(i,) for i in range(1, questions + 1)]
+        conn = psycopg2.connect(DB_CONN)
+        cur = conn.cursor()
+        cur.executemany(sql.SQL(query).format(sql.Identifier(table_name)), placeholder_list)
+        conn.commit()
+        cur.close()
+        conn.close()
+        print(f"{table_name} table seeded successfully, {placeholder_list[-1][0]} records inserted.")
+    except Exception:
+        print(f"seeding of {table_name} unsuccessful.")
+
+    
+def sql_delete_table():
+    try:
+        query = "drop table {};"
+        table_name = TABLE_NAME
+        conn = psycopg2.connect(DB_CONN)
+        cur = conn.cursor()
+        cur.execute(sql.SQL(query).format(sql.Identifier(table_name)))
+        conn.commit()
+        cur.close()
+        conn.close()
+        print(f"{table_name} table deleted.")
+    except Exception:
+        print(f"{table_name} did not exist.")
+
+def sql_count_total():
+    query = "select count(*) from {};"
+    table_name = TABLE_NAME
+    conn = psycopg2.connect(DB_CONN)
+    cur = conn.cursor()
+    cur.execute(sql.SQL(query).format(sql.Identifier(table_name)))
+    result = cur.fetchone()
+    cur.close()
+    conn.close()
+    return result
+
+def sql_count_remaining():
+    query = "select count(*) from {} where difficulty = 0;"
+    table_name = TABLE_NAME
+    conn = psycopg2.connect(DB_CONN)
+    cur = conn.cursor()
+    cur.execute(sql.SQL(query).format(sql.Identifier(table_name)))
+    result = cur.fetchone()
+    cur.close()
+    conn.close()
+    return result
+
+def sql_get_row(row_id):
+    query = "select * from {} where question_id = %s;"
+    placeholder = (row_id,)
+    table_name = TABLE_NAME
+    conn = psycopg2.connect(DB_CONN)
+    cur = conn.cursor()
+    cur.execute(sql.SQL(query).format(sql.Identifier(table_name)), placeholder)
+    result = cur.fetchone()
+    cur.close()
+    conn.close()
+    return result
+
+def sql_update(row_id, difficulty, correct):
+    query = "update {} set difficulty = %s, correct = %s, date = %s where question_id = %s;"
+    table_name = TABLE_NAME
+    placeholder = (difficulty, correct, datetime.now(), row_id)
+    conn = psycopg2.connect(DB_CONN)
+    cur = conn.cursor()
+    cur.execute(sql.SQL(query).format(sql.Identifier(table_name)), placeholder)
+    conn.commit()
+    cur.close()
+    conn.close()
--- src/main.py
+++ src/main.py
@@ -1,80 +1,43 @@
-import os
+from src.database import sql_count_total, sql_count_remaining, sql_update, sql_get_row, sql_create_table, sql_delete_table, sql_seed_table
+from datetime import datetime
+from fastapi import FastAPI
 
 import sys
-
-from fastapi import FastAPI, HTTPException
-
-from pydantic import BaseModel
-
-from datetime import datetime
-
-from dotenv import load_dotenv
-
-from src.utils import create_questions_string
-
-from src.database import create_database, seed_database, drop_database, update_database
-
-load_dotenv()
-
-DATABASE_URL = os.environ['DATABASE_URL']
 
 NUMBER_OF_QUESTIONS: int = 500
 
 EXAM_DATE: datetime = datetime.strptime('5 Jun 2025','%d %b %Y')
 
-questions_string = create_questions_string(NUMBER_OF_QUESTIONS)
-
 if "drop" in sys.argv:
-    if input("return 'Y' to confirm table deletion: ").lower() == 'y':
-        try:
-            drop_database(DATABASE_URL)
-        except:
-            print("exception1")
+    sql_delete_table()
 if "seed" in sys.argv:
-    try:
-        create_database(DATABASE_URL)
-    except:
-        print("exception2")
-    try:
-        seed_database(DATABASE_URL,questions_string)
-    except:
-        print("exception3")
-if len(sys.argv) != 1:
-    sys.exit()
-
-class Question(BaseModel):
-    question_id: int
-    difficulty: int
-    correct: bool
-    date: datetime
+    sql_create_table()
+    sql_seed_table(NUMBER_OF_QUESTIONS)
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return "Short question study tracker.\
-    difficulty range: 1 is easy 5 is hard 0 is unattempted"
+    return {"message": "Hello World"}
 
-# @app.get("/questions")
-# async def question_summary():
-#     questions_remaining = len([question for question in questions if question.split(",")[1] == "0"])
-#     time_remaining = EXAM_DATE - datetime.now()
-#     days_left = time_remaining.days
-#     questions_per_day = questions_remaining//days_left + 1
-#     return (f"questions_remaining: {questions_remaining}",
-#             f"days left: {days_left}",
-#             f"questions per day: {questions_per_day}")
+@app.get("/dashboard")
+async def dashboard():
+    questions_remaining, total_questions = sql_count_remaining()[0], sql_count_total()[0]
+    time_remaining = EXAM_DATE - datetime.now()
+    days_left = time_remaining.days
+    questions_per_day = questions_remaining//days_left + 1
+    return (f"questions attempted: {total_questions-questions_remaining}",
+            f"questions remaining: {questions_remaining}",
+            f"days left until exam: {days_left}",
+            f"questions per day: {questions_per_day}"
+            )
 
-# @app.get("/question/{question_id}")
-# async def question_id_call(question_id: int):
-#     if 0 < question_id < len(questions):
-#         return questions[question_id]
-#     raise HTTPException(status_code=404, detail="Question {question_id} not found")
+@app.get("/row/{row_id}")
+async def get_row_by_id(row_id):
+    result = sql_get_row(row_id)
+    return result
 
-@app.put("/question/{question_id}/{difficulty}/{correct}")
-async def question_update(question_id: int, difficulty: int, correct: bool):
-    if 0 < question_id <= NUMBER_OF_QUESTIONS:
-        question_update_list = [question_id,difficulty,correct,datetime.now()]
-        update_database(DATABASE_URL, question_update_list)
-        return f"Question {question_id} was updated to {question_update_list[1:]}"
-    raise HTTPException(status_code=404, detail="Question {question_id} not found")
+@app.put("/update/{row_id}/{difficulty}/{correct}")
+async def update_row(row_id, difficulty, correct):
+    sql_update(row_id, difficulty, correct)
+    return 200
